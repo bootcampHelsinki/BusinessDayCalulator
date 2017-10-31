96369cc480d75892c96d92851c2ca33cf9494724
Eskindir Abdela <eskindir@onsight.fi>
2017-10-31 21:46:33 +0200

    Add project files.

---
+++ BusinessDayCalculation.Tests/CalculationTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BusinessDayCalculation.Tests
+{
+    public class CalculationTests
+    {
+        [Theory]
+        [MemberData(nameof(DateFormats))]
+        public void Test_Different_Date_Formats_Are_Parsed(string dateFormat)
+        {
+            //arrange
+            var sut = new BusinessDayCalculation();
+            //act
+            var date = sut.ParseDate(dateFormat);
+            //assert
+            Assert.IsType<DateTime>(date);
+        }
+
+        [Fact]
+        public void Test_Business_Day_Calculation()
+        {
+            //arrange
+
+            var sut = new BusinessDayCalculation();
+
+            //act
+            var businessDays = sut.GetBusinessDays("11/11/2017", "11/11/2018");
+
+            //assert
+            Assert.Equal(313, businessDays);
+        }
+
+        [Fact]
+        public void Test_Business_Day_Calculation_Throws_Exception_When_Start_Date_is_Greater()
+        {
+            //arrange
+
+            var sut = new BusinessDayCalculation();
+
+            //assert
+            Assert.Throws<Exception>(() =>
+            {
+                sut.GetBusinessDays("11/11/2018", "11/11/2017");
+            });
+        }
+
+        public static IEnumerable<object[]> DateFormats => new List<object[]>
+        {
+            new object[]
+            {
+                "11/23/2017"
+            },
+            new object[]
+            {
+                "23.11.2017"
+            },
+            new object[]
+            {
+                "23/11/2017"
+            }
+        };
+    }
+}
---
+++ BusinessDayCalculation/BusinessDayCalculation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+
+namespace BusinessDayCalculation
+{
+    public class BusinessDayCalculation
+    {
+
+        public int GetBusinessDays(string startDateString, string endDateString)
+        {
+            var startDate = ParseDate(startDateString);
+            var endDate = ParseDate(endDateString);
+
+            if (startDate > endDate)
+            {
+                throw new Exception($"start date: {startDate} is greater than {endDate}");
+            }
+
+            var dateDiff = (endDate - startDate).TotalDays + 1;
+            var businessDayCount = 0;
+
+            for (int day = 0; day < dateDiff; day++)
+            {
+                if (day == 0)
+                {
+                    var weekDayStartDate = (int)startDate.DayOfWeek;
+
+                    if (weekDayStartDate != 0)
+                    {
+                        businessDayCount++;
+                    }
+                    continue;
+                }
+
+                var vacationDay = startDate.AddDays(day);
+
+                if (vacationDay.DayOfWeek != 0)
+                {
+                    businessDayCount++;
+                }
+            }
+
+            return businessDayCount;
+        }
+
+        public DateTime ParseDate(string dateFormat)
+        {
+            if (string.IsNullOrEmpty(dateFormat))
+            {
+                throw new ArgumentNullException(nameof(dateFormat));
+            }
+
+            DateTime date;
+
+            if (DateTime.TryParseExact(dateFormat, "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.AdjustToUniversal, out date ))
+            {
+                return date;
+            }
+
+            if (DateTime.TryParseExact(dateFormat, "dd.MM.yyyy", new CultureInfo("en-US"), DateTimeStyles.AdjustToUniversal, out date))
+            {
+                return date;
+            }
+
+            if (DateTime.TryParseExact(dateFormat, "dd/MM/yyyy", new CultureInfo("en-US"), DateTimeStyles.AdjustToUniversal, out date))
+            {
+                return date;
+            }
+
+            throw new Exception($"failed to parse date format {dateFormat}");
+        }
+    }
+}
